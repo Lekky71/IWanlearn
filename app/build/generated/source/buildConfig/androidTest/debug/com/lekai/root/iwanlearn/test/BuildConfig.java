adfc9f9bbb665e1033a14675dcc4efb4a1c8343e
Oluwalekae <hashilekky@gmail.com>
2017-03-07 01:19:52 +0100

    just here

---
+++ app/build/generated/source/buildConfig/androidTest/debug/com/lekai/root/iwanlearn/test/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.lekai.root.iwanlearn.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.lekai.root.iwanlearn.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
